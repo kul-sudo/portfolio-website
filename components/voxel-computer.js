0c210b245a49a7555188285418e492cb95b3fb90
kul-sudo <ulyanov.kirill@protonmail.com>
2022-08-31 11:10:33 +0300

    3d

---
+++ components/voxel-computer.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import { Suspense } from "react"
+import { Canvas, useLoader } from "@react-three/fiber"
+import { Environment, OrbitControls } from "@react-three/drei"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+
+const Model = () => {
+    // location of the 3D model
+    const gltf = useLoader(GLTFLoader, "computer_mouse.glb");
+    return (
+        <>
+            {/* Use scale to control the size of the 3D model */}
+            <primitive object={gltf.scene} scale={0.2} />
+        </>
+    );
+};
+
+const VoxelComputer = () => {
+    return (
+        <div>
+            <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 20 }} style={{height: '250px'}}>
+                <ambientLight intensity={0.7} />
+                <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow/>
+                <Suspense fallback={null}>
+                    <Model />
+                    <Environment preset="city" />
+                </Suspense>
+                <OrbitControls autoRotate />
+            </Canvas>
+        </div>
+    )
+  }
+
+export default VoxelComputer
+  
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
+import VoxelComputer from '../components/voxel-computer'
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <Head>
@@ -9,11 +10,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico"/>
       </Head>
 
-      <div className="flex justify-center mt-8">
+      <div className="z-[1] mt-[-2rem]">
+        <VoxelComputer></VoxelComputer>
+      </div>
+
+      <div className="flex justify-center mt-[-1rem]">
         <span className="text-7xl text-white text-center font-quicksand select-none">Cyril</span>
       </div>
 
-      <div className="830px:flex 830px:justify-center mt-[4rem] font-quicksand 830px:mt-7">
+      <div className="830px:flex 830px:justify-center mt-[4rem] font-quicksand 830px:mt-8">
         <div className="flex justify-center items-center before-830px:space-x-[15rem] 830px:block 830px:space-y-[1rem]">
           <div className="flex justify-center items-center align-middle select-none w-[13rem] h-[16rem] rounded-[0.3rem] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 skew-x-[15deg]">
             <div className="absolute justify-center items-center align-middle w-[18rem] h-[12rem] bg-transculent backdrop-blur-[0.625rem] rounded-[0.3rem]">
@@ -40,7 +45,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex justify-center mt-[4rem] 830px:mt-[1rem] font-quicksand">
+      <div className="flex justify-center mt-[4rem] 830px:mt-[1rem] font-quicksand z-[0]">
         <div className="flex justify-center items-center align-middle select-none w-[13rem] h-[16rem] rounded-[0.3rem] bg-gradient-to-r from-[#4dff03] to-[#00d0ff] skew-x-[15deg]">
           <div className="absolute justify-center items-center align-middle w-[18rem] h-[12rem] bg-transculent backdrop-blur-[0.625rem] rounded-[0.3rem]">
             <div className="flex justify-center text-center mt-6">
@@ -55,3 +60,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
