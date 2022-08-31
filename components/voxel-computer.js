bfa655c297d8a73803f909625726f43b55488c48
kul-sudo <ulyanov.kirill@protonmail.com>
2022-08-31 23:34:58 +0300

    updated selection

--- components/voxel-computer.js
+++ components/voxel-computer.js
@@ -4,29 +4,27 @@ import { Environment, OrbitControls } from "@react-three/drei"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 const Model = () => {
-    // location of the 3D model
     const gltf = useLoader(GLTFLoader, "computer_mouse.glb");
     return (
         <>
-            {/* Use scale to control the size of the 3D model */}
-            <primitive object={gltf.scene} scale={0.25} />
+            <primitive object={gltf.scene} scale={0.25}/>
         </>
     );
 };
 
 const VoxelComputer = () => {
     return (
-        <div>
-            <Canvas shadows dpr={[1, 2]} camera={{ position: [2, 5, 4], fov: 10 }} style={{height: '200px'}}>
-                <ambientLight intensity={0.7} />
+        <>
+            <Canvas shadows dpr={[1, 2]} camera={{ position: [2, 5, 4], fov: 10 }} style={{height: '200px'}} id="mouse">
+                <ambientLight intensity={0.7}/>
                 <spotLight intensity={0.5} angle={0.1} penumbra={1} castShadow/>
                 <Suspense fallback={null}>
-                    <Model />
-                    <Environment preset="city" />
+                    <Model/>
+                    <Environment preset="city"/>
                 </Suspense>
                 <OrbitControls autoRotate enableZoom={false} enablePan={false}/>
             </Canvas>
-        </div>
+        </>
     )
   }
 
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import VoxelComputer from '../components/voxel-computer'
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>Cyril&apos;s portfolio</title>
         <meta name="description" content="My personal cool-looking portfolio"/>
@@ -12,6 +12,10 @@ const Home = () => {
 
       <div className="z-[1]">
         <VoxelComputer></VoxelComputer>
+      </div>
+      
+      <div className="fixed bottom-0 right-0 mb-5 mr-5 z-[1]">
+        <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48" fill="white"><path d="M24 34q-4.15 0-7.075-2.925T14 24q0-4.15 2.925-7.075T24 14q4.15 0 7.075 2.925T34 24q0 4.15-2.925 7.075T24 34ZM3.5 25.5q-.65 0-1.075-.425Q2 24.65 2 24q0-.65.425-1.075Q2.85 22.5 3.5 22.5h5q.65 0 1.075.425Q10 23.35 10 24q0 .65-.425 1.075-.425.425-1.075.425Zm36 0q-.65 0-1.075-.425Q38 24.65 38 24q0-.65.425-1.075.425-.425 1.075-.425h5q.65 0 1.075.425Q46 23.35 46 24q0 .65-.425 1.075-.425.425-1.075.425ZM24 10q-.65 0-1.075-.425Q22.5 9.15 22.5 8.5v-5q0-.65.425-1.075Q23.35 2 24 2q.65 0 1.075.425.425.425.425 1.075v5q0 .65-.425 1.075Q24.65 10 24 10Zm0 36q-.65 0-1.075-.425-.425-.425-.425-1.075v-5q0-.65.425-1.075Q23.35 38 24 38q.65 0 1.075.425.425.425.425 1.075v5q0 .65-.425 1.075Q24.65 46 24 46ZM12 14.1l-2.85-2.8q-.45-.45-.425-1.075.025-.625.425-1.075.45-.45 1.075-.45t1.075.45L14.1 12q.4.45.4 1.05 0 .6-.4 1-.4.45-1.025.45-.625 0-1.075-.4Zm24.7 24.75L33.9 36q-.4-.45-.4-1.075t.45-1.025q.4-.45 1-.45t1.05.45l2.85 2.8q.45.45.425 1.075-.025.625-.425 1.075-.45.45-1.075.45t-1.075-.45ZM33.9 14.1q-.45-.45-.45-1.05 0-.6.45-1.05l2.8-2.85q.45-.45 1.075-.425.625.025 1.075.425.45.45.45 1.075t-.45 1.075L36 14.1q-.4.4-1.025.4-.625 0-1.075-.4ZM9.15 38.85q-.45-.45-.45-1.075t.45-1.075L12 33.9q.45-.45 1.05-.45.6 0 1.05.45.45.45.45 1.05 0 .6-.45 1.05l-2.8 2.85q-.45.45-1.075.425-.625-.025-1.075-.425Z"/></svg>
       </div>
 
       <div className="flex justify-center backdrop-blur-[0.625rem] z-[0]">
@@ -36,7 +40,7 @@ const Home = () => {
               <div className="flex justify-center text-center mt-6">
                 <span className="font-[700] skew-x-[-15deg]">My projects</span>
               </div>
-              <div className="flex items-center justify-center text-center align-middle mt-5">
+              <div className="flex items-center justify-center text-center align-middle mt-7">
                 <span className="skew-x-[-15deg]">
                   <a href="https://github.com/kul-sudo/eportal" rel="noreferrer" target="_blank" className="underline">eportal</a><br/>
                   <a href="https://github.com/kul-sudo/Smiling-Zombies" rel="noreferrer" target="_blank" className="underline">smiling zombies</a>
@@ -53,7 +57,7 @@ const Home = () => {
             <div className="flex justify-center text-center mt-6">
               <span className="font-[700] skew-x-[-15deg]">Soft skills</span>
             </div>
-            <div className="flex items-center justify-center text-center align-middle mt-6">
+            <div className="flex items-center justify-center text-center align-middle mt-8">
               <span className="flex skew-x-[-15deg]">
                 Solid English<br/>
                 Stress tolerance
@@ -62,7 +66,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
