9d06394ca4dd4e4e3a088dbd943c513807b2e30e
kul-sudo <ulyanov.kirill@protonmail.com>
2022-08-31 17:28:32 +0300

    'updated

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const Home = () => {
 
       <div className="830px:flex 830px:justify-center mt-[3rem] font-quicksand 830px:mt-8 z-[0]">
         <div className="flex justify-center items-center before-830px:space-x-[15rem] 830px:block 830px:space-y-[1rem]">
-          <div className="flex justify-center items-center align-middle select-none w-[13rem] h-[16rem] rounded-[0.3rem] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 skew-x-[15deg]">
+          <div className="flex justify-center items-center align-middle w-[13rem] h-[16rem] rounded-[0.3rem] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 skew-x-[15deg]">
             <div className="absolute justify-center items-center align-middle w-[18rem] h-[12rem] bg-transculent backdrop-blur-[0.625rem] rounded-[0.3rem]">
               <div className="flex justify-center text-center mt-6">
                 <span className="font-[700] skew-x-[-15deg]">Technologies</span>
@@ -31,14 +31,16 @@ const Home = () => {
             </div>
           </div>
 
-
-          <div className="flex justify-center items-center align-middle select-none w-[13rem] h-[16rem] rounded-[0.3rem] bg-gradient-to-r from-pink-500 to-yellow-500 skew-x-[15deg]">
+          <div className="flex justify-center items-center align-middle w-[13rem] h-[16rem] rounded-[0.3rem] bg-gradient-to-r from-pink-500 to-yellow-500 skew-x-[15deg]">
             <div className="absolute justify-center items-center align-middle w-[18rem] h-[12rem] bg-transculent backdrop-blur-[0.625rem] rounded-[0.3rem]">
               <div className="flex justify-center text-center mt-6">
-                <span className="font-[700] skew-x-[-15deg]">Properties</span>
+                <span className="font-[700] skew-x-[-15deg]">My projects</span>
               </div>
-              <div className="flex items-center justify-center text-center align-middle mt-6">
-                <span className="flex skew-x-[-15deg]">Name: Cyril<br/>Sex: male<br/>Fave food: sashimi</span>
+              <div className="flex items-center justify-center text-center align-middle mt-5">
+                <span className="skew-x-[-15deg]">
+                  <a href="https://github.com/kul-sudo/eportal" target="_blank" className="underline">eportal</a><br/>
+                  <a href="https://github.com/kul-sudo/Smiling-Zombies" target="_blank" className="underline">smiling zombies</a>
+                </span>
               </div>
             </div>
           </div>
@@ -46,13 +48,16 @@ const Home = () => {
       </div>
 
       <div className="flex justify-center mt-[4rem] 830px:mt-[1rem] font-quicksand">
-        <div className="flex justify-center items-center align-middle select-none w-[13rem] h-[16rem] rounded-[0.3rem] bg-gradient-to-r from-[#4dff03] to-[#00d0ff] skew-x-[15deg]">
+        <div className="flex justify-center items-center align-middle w-[13rem] h-[16rem] rounded-[0.3rem] bg-gradient-to-r from-[#4dff03] to-[#00d0ff] skew-x-[15deg]">
           <div className="absolute justify-center items-center align-middle w-[18rem] h-[12rem] bg-transculent backdrop-blur-[0.625rem] rounded-[0.3rem]">
             <div className="flex justify-center text-center mt-6">
-              <span className="font-[700] skew-x-[-15deg]">My values</span>
+              <span className="font-[700] skew-x-[-15deg]">Soft skills</span>
             </div>
             <div className="flex items-center justify-center text-center align-middle mt-6">
-              <span className="flex skew-x-[-15deg]">Follow the deadline<br/>Follow the law<br/>A good reputation<br/>Not let people down</span>
+              <span className="flex skew-x-[-15deg]">
+                Glorious English<br/>
+                Stress tolerance
+              </span>
             </div>
           </div>
         </div>
