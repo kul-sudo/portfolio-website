8624978f6ed01eb7727efc8d6427a5602aed8934
kul-sudo <ulyanov.kirill@protonmail.com>
2022-09-04 11:24:54 -0700

    dark mode and bright mode

--- components/voxel-computer.js
+++ components/voxel-computer.js
@@ -3,10 +3,14 @@ import { Canvas, useLoader } from '@react-three/fiber'
 import { Environment, OrbitControls } from '@react-three/drei'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { Html } from '@react-three/drei'
-import CircularProgress from '@mui/material/CircularProgress';
 
-function Loader() {
-  return <Html center><CircularProgress variant="determine"></CircularProgress></Html>
+const Loader = () => {
+  return <Html center>
+    <svg className="animate-spin h-5 w-5 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
+  </Html>
 }
 
 const Model = () => {
@@ -36,4 +40,3 @@ const GraphicsComputer = () => {
   }
 
 export default GraphicsComputer
-  
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import GraphicsComputer from '../components/voxel-computer'
 import useTheme from '../lib/theme'
 import { useEffect } from 'react'
+import { SunIcon, MoonIcon } from '@chakra-ui/icons'
+import { IconButton } from '@chakra-ui/react'
 
 const Home = () => {
   const sun = "M10,7.5c1.38,0,2.5,1.12,2.5,2.5s-1.12,2.5-2.5,2.5S7.5,11.38,7.5,10S8.62,7.5,10,7.5z M10,6c-2.21,0-4,1.79-4,4s1.79,4,4,4 s4-1.79,4-4S12.21,6,10,6L10,6z M3.75,10.75c0.41,0,0.75-0.34,0.75-0.75c0-0.41-0.34-0.75-0.75-0.75h-2C1.34,9.25,1,9.59,1,10 s0.34,0.75,0.75,0.75H3.75z M18.25,10.75c0.41,0,0.75-0.34,0.75-0.75c0-0.41-0.34-0.75-0.75-0.75h-2c-0.41,0-0.75,0.34-0.75,0.75 s0.34,0.75,0.75,0.75H18.25z M9.25,3.75C9.25,4.16,9.59,4.5,10,4.5c0.41,0,0.75-0.34,0.75-0.75v-2C10.75,1.34,10.41,1,10,1 S9.25,1.34,9.25,1.75V3.75z M13.89,5.05c-0.29,0.29-0.29,0.77,0,1.06s0.77,0.29,1.06,0l1.06-1.06c0.29-0.29,0.29-0.77,0-1.06 c-0.29-0.29-0.77-0.29-1.06,0L13.89,5.05z M3.99,14.95c-0.29,0.29-0.29,0.77,0,1.06s0.77,0.29,1.06,0l1.06-1.06 c0.29-0.29,0.29-0.77,0-1.06c-0.29-0.29-0.77-0.29-1.06,0L3.99,14.95z M5.05,6.11c0.29,0.29,0.77,0.29,1.06,0s0.29-0.77,0-1.06 L5.05,3.99c-0.29-0.29-0.77-0.29-1.06,0s-0.29,0.77,0,1.06L5.05,6.11z M14.95,16.01c0.29,0.29,0.77,0.29,1.06,0s0.29-0.77,0-1.06 l-1.06-1.06c-0.29-0.29-0.77-0.29-1.06,0c-0.29,0.29-0.29,0.77,0,1.06L14.95,16.01z M9.25,18.25C9.25,18.66,9.59,19,10,19 c0.41,0,0.75-0.34,0.75-0.75v-2c0-0.41-0.34-0.75-0.75-0.75s-0.75,0.34-0.75,0.75V18.25z"
@@ -13,17 +15,19 @@ const Home = () => {
   useEffect(() => {
     if (dark) {
       document.querySelector('html').classList.add('bright')
-      document.getElementById('change-theme-svg').classList.remove('change-theme-svg-bright')
-      document.getElementById('change-theme-svg-path').setAttribute('d', sun)
       document.getElementById('caption').style.color = 'black'
+      document.getElementById('github').style.color = 'black'
+      document.getElementById('sun').style.display = 'none'
+      document.getElementById('moon').style.display = 'block'
     } else {
       document.querySelector('html').classList.remove('bright')
-      document.getElementById('change-theme-svg').classList.add('change-theme-svg-bright')
-      document.getElementById('change-theme-svg-path').setAttribute('d', moon)
       document.getElementById('caption').style.color = 'white'
+      document.getElementById('github').style.color = 'white'
+      document.getElementById('sun').style.display = 'block'
+      document.getElementById('moon').style.display = 'none'
     }
   }, [dark])
-  
+
   return (
     <>
       <Head>
@@ -36,10 +40,17 @@ const Home = () => {
         <GraphicsComputer></GraphicsComputer>
       </div>
       
-      <div className="fixed bottom-0 right-0 mb-5 mr-5 z-[1]" onClick={toggleDarkMode} id="dark-mode-toggle">
-        <svg id="change-theme-svg" xmlns="http://www.w3.org/2000/svg" height="48" width="48">
-          <path id="change-theme-svg-path"/>
-        </svg>
+      <div className="flex justify-center align-middle fixed bottom-10 right-10 z-[1] 830px:block">
+        <a className="font-quicksand" href="https://github.com/kul-sudo" id="github" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <div className="flex justify-center before-830px:ml-3 mt-[0.05rem]">
+          <div id="sun">
+            <IconButton onClick={toggleDarkMode} icon={<SunIcon/>} style={{width: '20px', height: '20px', color: 'white'}}/>
+          </div>
+
+          <div id="moon">
+            <IconButton onClick={toggleDarkMode} icon={<MoonIcon/>} style={{width: '20px', height: '20px', color: 'black'}}/>
+          </div>
+        </div>
       </div>
 
       <div className="flex justify-center backdrop-blur-[0.625rem] z-[0]">
@@ -79,7 +90,7 @@ const Home = () => {
         <div className="flex justify-center items-center align-middle w-[13rem] h-[16rem] rounded-[0.3rem] bg-gradient-to-r from-[#4dff03] to-[#00d0ff] skew-x-[15deg]">
           <div className="absolute justify-center items-center align-middle w-[18rem] h-[12rem] bg-transculent backdrop-blur-[0.625rem] rounded-[0.3rem]">
             <div className="flex justify-center text-center mt-6">
-              <span className="font-[700] skew-x-[-15deg]">Soft skills</span>
+              <span className="font-[700] skew-x-[-15deg]">Hard skills</span>
             </div>
             <div className="flex items-center justify-center text-center align-middle mt-8">
               <span className="flex skew-x-[-15deg]">
