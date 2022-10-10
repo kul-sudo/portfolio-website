c046b51801004afec78d7e3e142f706df22a8d66
kul-sudo <ulyanov.kirill@protonmail.com>
2022-10-10 23:28:04 +0300

    feat(mousegraphics.js): the 3d mouse can now preload

--- components/mouseGraphics.js
+++ components/mouseGraphics.js
@@ -2,12 +2,12 @@ import { Suspense } from 'react'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { Environment, OrbitControls } from '@react-three/drei'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { Html } from '@react-three/drei'
+import { Html, Preload } from '@react-three/drei'
 import { Stack, Spinner } from '@chakra-ui/react'
 
 const Model = () => {
   return (
-	  <primitive object={useLoader(GLTFLoader, '/computer_mouse.glb').scene} scale={0.25} resolution={2} />
+	  <primitive object={useLoader(GLTFLoader, '/computer_mouse.glb').scene} scale={0.25} />
   );
 };
 
@@ -17,9 +17,10 @@ const GraphicsMouse = () => {
       <Canvas camera={{ position: [2, 5, 4], fov: 10 }} style={{width: "181px", height: "200px"}} id="mouse">
         <ambientLight />
         <spotLight />
-        <Suspense fallback={<Html center><Spinner boxSize={50}/></Html>}>
+        <Suspense fallback={<Html center><Spinner boxSize={50} /></Html>}>
           <Model />
-          <Environment preset="city"/>
+          <Preload all />
+          <Environment preset="city" />
         </Suspense>
         <OrbitControls autoRotate enableZoom={false} enablePan={false} />
       </Canvas>
