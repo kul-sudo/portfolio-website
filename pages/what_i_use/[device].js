a8be895f5041fab8154a1c57b3e1124b36851cf5
kul-sudo <ulyanov.kirill@protonmail.com>
2022-10-11 22:08:56 +0300

    fix: test
    n

--- pages/what_i_use/[device].js
+++ pages/what_i_use/[device].js
@@ -1,12 +1,11 @@
 import { useRouter } from 'next/router'
 import { Text } from '@chakra-ui/react'
+import devices_list from '../../lib/devices_list'
 
-const Device = props => {
-
+const Device = () => {
   return (
     <Text>ff</Text>
   )
 }
-
 
 export default Device
