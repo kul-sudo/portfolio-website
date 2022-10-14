3f995ca325e6684a6d43c2e7165bef2141ad86f3
kul-sudo <ulyanov.kirill@protonmail.com>
2022-10-14 22:39:55 +0300

    fix(next.config.js): ignore eslint

--- pages/what_i_use.js
+++ pages/what_i_use.js
@@ -20,7 +20,7 @@ const WhatIUse = () => {
 
       <Center mt="5rem">
         <Grid templateColumns="repeat(2, 1fr)" templateRows="repeat(2, 1fr)" rowGap="5rem" columnGap="10rem" id="use-items">
-          {Object.keys(Paths).map((key, index) => (
+          {Object.keys(Paths).map((key) => (
             <NextLink href={"what_i_use/"+key}>
               <Box borderRadius="5%" w="16rem" bg="#181818" _hover={{transform: "translateY(-3px)"}} transition="0.2s">
                 <Image 
