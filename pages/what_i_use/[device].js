fe4ceae15c3d8af62572cf8cf0175c3f697c83f7
kul-sudo <ulyanov.kirill@protonmail.com>
2022-10-14 18:46:32 +0300

    fix(lib/what_i_use.js, lib/devices_list.js): removed unneeded files

--- pages/what_i_use/[device].js
+++ pages/what_i_use/[device].js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 import { Stack, Text } from '@chakra-ui/react'
+import Navbar from '../../components/Navbar'
 import '@fontsource/quicksand/600.css'
 
 const DevicePage = () => {
@@ -15,19 +16,22 @@ const DevicePage = () => {
   }
   
   return (
-    <Stack display="flex" justifyContent="center" alignItems="center" mt="5rem">
-      <Image 
-        src={paths[device].at(1)} 
-        alt="mouse"
-        width="270%"
-        height="190%"
-        draggable={false}
-        loading="eager"
-        quality="100"
-        style={{borderRadius: "5%", userSelect: "none"}}
-        />
-      <Text fontSize="3rem">{paths[device].at(0)}</Text>
-    </Stack>
+    <>
+      <Navbar />
+      <Stack display="flex" justifyContent="center" alignItems="center" mt="5rem">
+        <Image 
+          src={paths[device].at(1)} 
+          alt="mouse"
+          width="270%"
+          height="190%"
+          draggable={false}
+          loading="eager"
+          quality="100"
+          style={{borderRadius: "5%", userSelect: "none"}}
+          />
+        <Text fontSize="3rem">{paths[device].at(0)}</Text>
+      </Stack>
+    </>
   )
 }
 
