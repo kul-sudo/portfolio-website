a39b3c7bda78efb3d6f048fda840bbeb8676d119
kul-sudo <ulyanov.kirill@protonmail.com>
2022-10-27 23:39:09 +0300

    feat: icon added

--- pages/what_i_use.js
+++ pages/what_i_use.js
@@ -9,7 +9,6 @@ const WhatIUse = () => {
     <>
       <Head>
         <title>Cyril Tasman</title>
-        <meta name="description" content="My personal portfolio" />
         <link rel="icon" href="https://res.cloudinary.com/dsliut4oh/image/upload/v1666902603/logo_sta6iy.png" />
       </Head>
 
