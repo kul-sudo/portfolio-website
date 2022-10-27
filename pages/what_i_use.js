c1c2b8396e02d90680f4a85441d7d9745ff55a5d
kul-sudo <ulyanov.kirill@protonmail.com>
2022-10-27 21:00:23 +0300

    feat: new colormode

--- pages/what_i_use.js
+++ pages/what_i_use.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import NextLink from 'next/link'
-import { Grid, Center, Box, Divider, Link, Image } from '@chakra-ui/react'
+import { Grid, Center, Box, Divider, Link, Image, useColorModeValue } from '@chakra-ui/react'
 import Paths from '../lib/device_posts.json'
 import Navbar from '../components/Navbar'
 import '@fontsource/quicksand/600.css'
@@ -20,7 +20,7 @@ const WhatIUse = () => {
         <Grid templateColumns="repeat(2, 1fr)" templateRows="repeat(2, 1fr)" rowGap="5rem" columnGap="10rem" id="use-items">
           {Object.keys(Paths).map((key) => (
             <NextLink href={"what_i_use/"+key}>
-              <Box borderRadius="5%" w="16rem" bg="#181818" _hover={{transform: "translateY(-3px)"}} transition="0.2s" cursor="pointer">
+              <Box borderRadius="5%" w="16rem" bg={useColorModeValue('#212121', '#e3e3e3')} _hover={{transform: "translateY(-3px)"}} transition="0.2s" cursor="pointer">
                 <Image
                   src={Paths[key][1]} 
                   alt="device"
@@ -29,7 +29,7 @@ const WhatIUse = () => {
                 />
                 <Divider />
                 <Box display="flex" justifyContent="center" h="5rem">
-                  <Link color="white" alignSelf="center" textAlign="center" fontSize="1.5rem" fontWeight="600">{Paths[key][0]}</Link>
+                  <Link color={useColorModeValue('#ffffff', '#000000')} alignSelf="center" textAlign="center" fontSize="1.5rem" fontWeight="600">{Paths[key][0]}</Link>
                 </Box>
               </Box>
             </NextLink>
