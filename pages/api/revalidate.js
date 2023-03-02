18c17aa1d4c9111dd6d7743f18c4313f6c866ad6
kul-sudo <ulyanov.kirill@protonmail.com>
2023-03-02 20:32:21 +0300

    feat: test

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,10 +1,17 @@
 export default async function handler(req, res) {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+
   try {
-    await res.revalidate(req.query.path);
-    return res.json({
-      revalidated: true
-    })
+    // this should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    await res.revalidate('/blog/g_pro_wireless')
+    return res.json({ revalidated: true })
   } catch (err) {
-    return res.status(500).send('Error revalidating');
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating')
   }
 }
--- pages/blog/[postname].jsx
+++ pages/blog/[postname].jsx
@@ -151,7 +151,7 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
