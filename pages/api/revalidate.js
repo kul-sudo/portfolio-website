3d14c8a0838f4052914e86c58152e2a82220ae7c
kul-sudo <ulyanov.kirill@protonmail.com>
2023-03-02 20:41:05 +0300

    feat: test

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,8 +1,4 @@
 export default async function handler(req, res) {
-  if (req.query.secret !== process.env.SECRET_TOKEN) {
-    return res.status(401).json({ message: 'Invalid token' })
-  }
-
   try {
     await res.revalidate('/blog/g_pro_wireless')
     return res.json({ revalidated: true })
